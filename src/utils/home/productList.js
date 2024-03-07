c645977499eb6d5e4ee3c8467d986f2e4dfcf55e
AkbarNurDzikri <dzikrinurakbar94@gmail.com>
2024-03-07 17:15:02 +0700

    prepare deploy

--- src/utils/home/productList.js
+++ src/utils/home/productList.js
@@ -11,6 +11,22 @@ const productList = [
     categoryName: "Hot Fill Sachet",
     pathImage: "/img/home/our-products/plc1.svg",
   },
+  {
+    categoryName: "Tisseu",
+    pathImage: "/img/home/our-products/plc1.svg",
+  },
+  {
+    categoryName: "Shampoo",
+    pathImage: "/img/home/our-products/plc1.svg",
+  },
+  {
+    categoryName: "Liqud",
+    pathImage: "/img/home/our-products/plc1.svg",
+  },
+  {
+    categoryName: "Pharmacy",
+    pathImage: "/img/home/our-products/plc1.svg",
+  },
 ];
 
 export default productList;
